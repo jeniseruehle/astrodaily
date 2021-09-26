91b7a5eeeb825d6b1dcb19ea29ffee927b228e93
Jenise Ruehle <jeniseruehle@gmail.com>
2021-09-26 12:14:15 -0500

    update reducer

--- astro-frontend/src/reducers/manageTransit.js
+++ astro-frontend/src/reducers/manageTransit.js
@@ -2,7 +2,8 @@ export default function manageTransit(state = {transits: []}, action) {
     switch(action.type) {
         case "LOAD_TRANSITS":
             return {...state, transits: action.payload}
-
+        case "ADD_TRANSITS":
+            return {...state, transits: [...state.transits, action.payload]}
         default:
             return state
     }
